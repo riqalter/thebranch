5010e9fd0a63b117384c0853a3d5af3dfafb7ba4
Riq <mikail.thoriq@gmail.com>
2021-12-23 23:42:57 +0700

    initial commit

---
+++ script/index.js
@@ -0,0 +1,12 @@
+import navbar from "./js/navbar.js";
+import jumbotron from "./js/jumbotron.js";
+import vaksinasi from "./js/vaksinasi.js";
+import faq from "./js/faq.js";
+
+// Init
+window.onload = () => {
+  navbar();
+  jumbotron();
+  vaksinasi();
+  faq();
+};
